619191595753818ffb613f5c0f10caa3a133420d
withered-magic <withering.magic@gmail.com>
2024-07-24 19:18:44 +0000

    .

--- kms/kms.go
+++ kms/kms.go
@@ -18,15 +18,21 @@ type PrivateKey struct {
 	ctx         context.Context
 	client      *kms.Client
 	keyId       string
+	keySpec     types.KeySpec
 	publicKey   crypto.PublicKey
 	publicKeyMu sync.RWMutex
 }
 
 func NewPrivateKey(ctx context.Context, client *kms.Client, keyId string) (*PrivateKey, error) {
+	output, err := client.DescribeKey(ctx, &kms.DescribeKeyInput{KeyId: aws.String(keyId)})
+	if err != nil {
+		return nil, err
+	}
 	key := &PrivateKey{
-		ctx:    ctx,
-		client: client,
-		keyId:  keyId,
+		ctx:     ctx,
+		client:  client,
+		keyId:   keyId,
+		keySpec: output.KeyMetadata.KeySpec,
 	}
 	return key, nil
 }
@@ -67,10 +73,14 @@ func (priv *PrivateKey) Public() crypto.PublicKey {
 }
 
 func (priv *PrivateKey) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
+	algo, err := priv.chooseSigningAlgorithm(opts)
+	if err != nil {
+		return nil, err
+	}
 	output, err := priv.client.Sign(priv.ctx, &kms.SignInput{
 		KeyId:            aws.String(priv.keyId),
 		Message:          digest,
-		SigningAlgorithm: types.SigningAlgorithmSpecRsassaPkcs1V15Sha256,
+		SigningAlgorithm: algo,
 		MessageType:      types.MessageTypeDigest,
 	})
 	if err != nil {
@@ -79,16 +89,32 @@ func (priv *PrivateKey) Sign(rand io.Reader, digest []byte, opts crypto.SignerOp
 	return output.Signature, nil
 }
 
-func chooseSigningAlgorithm(opts crypto.SignerOpts) (types.SigningAlgorithmSpec, error) {
+func (priv *PrivateKey) chooseSigningAlgorithm(opts crypto.SignerOpts) (types.SigningAlgorithmSpec, error) {
 	hashFunc := opts.HashFunc()
-	switch hashFunc {
-	case crypto.SHA256:
-		return types.SigningAlgorithmSpecEcdsaSha256, nil
-	case crypto.SHA384:
-		return types.SigningAlgorithmSpecEcdsaSha384, nil
-	case crypto.SHA512:
-		return types.SigningAlgorithmSpecEcdsaSha512, nil
+	switch priv.keySpec {
+	case types.KeySpecRsa2048, types.KeySpecRsa3072, types.KeySpecRsa4096:
+		switch hashFunc {
+		case crypto.SHA256:
+			return types.SigningAlgorithmSpecRsassaPkcs1V15Sha256, nil
+		case crypto.SHA384:
+			return types.SigningAlgorithmSpecRsassaPkcs1V15Sha384, nil
+		case crypto.SHA512:
+			return types.SigningAlgorithmSpecRsassaPkcs1V15Sha512, nil
+		default:
+			return "", fmt.Errorf("unsupported combination of key spec and hash func: %s, %s", priv.keySpec, hashFunc)
+		}
+	case types.KeySpecEccNistP256, types.KeySpecEccNistP384, types.KeySpecEccNistP521:
+		switch hashFunc {
+		case crypto.SHA256:
+			return types.SigningAlgorithmSpecEcdsaSha256, nil
+		case crypto.SHA384:
+			return types.SigningAlgorithmSpecEcdsaSha384, nil
+		case crypto.SHA512:
+			return types.SigningAlgorithmSpecEcdsaSha512, nil
+		default:
+			return "", fmt.Errorf("unsupported combination of key spec and hash func: %s, %s", priv.keySpec, hashFunc)
+		}
 	default:
-		return "", fmt.Errorf("unsupported hash func: %s", hashFunc)
+		return "", fmt.Errorf("unsupported key spec: %s", priv.keySpec)
 	}
 }
